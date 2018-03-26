a96024a86ba57ad5b513805a2f7f988a9eb6b390
Вишневецкий Сергей <Sergey.Vishnevecky@R23VISHNEVECKYI>
2018-03-26 13:39:43 +0300

    namespace

--- LogExplorer/LogLine.cs
+++ LogExplorer/LogLine.cs
@@ -1,31 +1,33 @@
 ﻿using System;
-
-public class LogLine
+namespace LogExplorer
 {
-
-    // TODO: требуется замена на фабрику
-
-    public int id { get; set; }
-    public string Name { get; set; }
-    public string Company { get; set; }
-    public string IP { get; set; }
-    public string Hash { get; set; }
-    public DateTime InDate { get; set; }
-    public DateTime OutDate { get; set; }
-    public bool Error { get; set; }
-
-
-    public LogLine(String LogString)
-    {
-        string[] LogLineArray = LogString.Split(";");
-        Name = LogLineArray[1];
-    }
-    public static Parse(String LogString)
+    public class LogLine
     {
 
+        // TODO: требуется замена на фабрику
+
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Company { get; set; }
+        public string IP { get; set; }
+        public string Hash { get; set; }
+        public DateTime InDate { get; set; }
+        public DateTime OutDate { get; set; }
+        public bool Error { get; set; }
+
+
+        public LogLine(String LogString)
+        {
+            string[] LogLineArray = LogString.Split(";");
+            Name = LogLineArray[1];
+        }
+        public static Parse(String LogString)
+        {
 
 
 
 
+
+        }
     }
 }
