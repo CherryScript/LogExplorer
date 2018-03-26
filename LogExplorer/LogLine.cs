01c8e2117b3d68f6775d680f3c5205c897bb8ca9
Вишневецкий Сергей <Sergey.Vishnevecky@R23VISHNEVECKYI>
2018-03-26 13:36:56 +0300

    LogLine

---
+++ LogExplorer/LogLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+public class LogLine
+{
+
+    // TODO: требуется замена на фабрику
+
+    public int id { get; set; }
+    public string Name { get; set; }
+    public string Company { get; set; }
+    public string IP { get; set; }
+    public string Hash { get; set; }
+    public DateTime InDate { get; set; }
+    public DateTime OutDate { get; set; }
+    public bool Error { get; set; }
+
+
+    public LogLine(String LogString)
+    {
+        string[] LogLineArray = LogString.Split(";");
+        Name = LogLineArray[1];
+    }
+    public static Parse(String LogString)
+    {
+
+
+
+
+
+    }
+}
--- LogExplorer/MainWindow.xaml.cs
+++ LogExplorer/MainWindow.xaml.cs
@@ -23,6 +23,11 @@ namespace LogExplorer
         public MainWindow()
         {
             InitializeComponent();
+
+            LogLine ll = new LogLine("178;Слуцкий Дмитрий Анатольевич;РНПК;192.168.13.213;c9c38e43-ca61-4b27-8943-538d4f579990;13.12.2017 20:46:50;13.12.2017 2:20:10;1;");
+            System.Console.Out(ll.Name);
+
+
         }
     }
 }
